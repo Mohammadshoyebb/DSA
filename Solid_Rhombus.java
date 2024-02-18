c9603d5dd372ddcb01b4176c8dd92453e3df16b6
Mohammadshoyebb <141633689+Mohammadshoyebb@users.noreply.github.com>
2024-02-18 18:47:42 +0530

    commit

---
+++ Solid_Rhombus.java
@@ -0,0 +1,13 @@
+public class Solid_Rhombus {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i=1; i<=5; i++){
+            for(int j = 1; j<=(n-i); j++){
+                System.out.print("  ");
+            }
+            System.out.print("* * * * *");
+            System.out.println();
+        }
+    }
+}
